e945dafc94d6ec00cc776fbec934eec7dbd47e77
MaxMamoSiri <94049777+MaxMamoSiri@users.noreply.github.com>
2022-04-10 08:56:45 +0200

    Creating the function to read the CSV file

--- functions.py
+++ functions.py
@@ -1,1 +1,17 @@
+import csv
+final_dict=[]
 
+def loadCSV():
+    with open('artists.csv', newline='') as csvfile:
+        reader = list(csv.reader(csvfile))
+        for row in reader[1:]:
+            temp_dict = {}
+            temp_dict["name_surname"] = row[0]
+            temp_dict["birthday"] = row[1]
+            temp_dict["gender"] = row[2]
+            temp_dict["nationality"] = row[3]
+            temp_dict["death"] = row[4]
+            temp_dict["movement"] = row[5]
+            final_dict.append(temp_dict)
+    return final_dict
+  

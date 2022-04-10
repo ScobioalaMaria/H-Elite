4e886e1b638185416cccd400e858168f3c37aefe
Corinne14 <94049706+Corinne14@users.noreply.github.com>
2022-04-10 09:26:32 +0200

    Creating class for artist

---
+++ artist.py
@@ -0,0 +1,9 @@
+class Artist:
+    def __init__(self, name_surname="", birthday="", gender="", nationality="",
+                 death="", movement=""):
+        self.name_surname = name_surname
+        self.birthday = birthday
+        self.gender = gender
+        self.nationality = nationality
+        self.death = death
+        self.movement = movement

e8f099189ae802df4330df3b3b335db2bd29b623
Corinne14 <94049706+Corinne14@users.noreply.github.com>
2022-04-29 09:44:40 +0200

    Created test on create object function

---
+++ tests/test_functions.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+import functions
+import artist
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_createObjectList(self):
+        actual = functions.createObjectList([{'name_surname': 'Claude Monet', 
+                                                'birthday': '14/11/1840', 
+                                                'gender': 'Male', 
+                                                'nationality': 'French',
+                                                'ydeath': '1926', 
+                                                'movement': 'Impressionism'
+        }])
+        oggetto = artist.Artist('Claude Monet','14/11/1840','Male','French','1926','Impressionism')
+        lista=[]
+        lista.append(oggetto)
+        assert lista[0].name_surname == actual[0].name_surname
+        
+
+if __name__ == '__main__':
+    unittest.main()
